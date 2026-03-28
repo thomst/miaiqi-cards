272d96c922da987ba911606d65f8e0022e378f4b
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-03-28 17:08:43 +0100

    added static-root config

--- miaiqi_cards/settings.py
+++ miaiqi_cards/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +125,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'miaiqi-cards/staticfiles/')
