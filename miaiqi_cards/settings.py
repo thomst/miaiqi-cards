8cbb1eb7441d8ed7616e73098aefe615638ebd50
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-03-28 17:10:21 +0100

    fixed static-root setup

--- miaiqi_cards/settings.py
+++ miaiqi_cards/settings.py
@@ -127,4 +127,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'miaiqi-cards/staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'miaiqi_cards/staticfiles/')
