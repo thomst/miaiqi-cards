b68ea307e48e373cbdb74bbb98b4e7b81d4f7c80
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-03-30 23:52:12 +0200

    improving layout

--- cards/urls.py
+++ cards/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.postcard_list, name='postcard_list'),
+    path('', views.welcome, name='welcome'),
+    path('galery/', views.postcard_list, name='postcard_list'),
     path('<int:pk>/', views.postcard_detail, name='postcard_detail'),
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,7 +1,12 @@
+import random
 from django.shortcuts import render, get_object_or_404
 from .models import Postcard
 
 # Create your views here.
+
+def welcome(request):
+    postcard = Postcard.objects.filter(is_public=True).first()
+    return render(request, 'cards/welcome.html', {'postcard': postcard})
 
 def postcard_list(request):
     postcards = Postcard.objects.filter(is_public=True)
