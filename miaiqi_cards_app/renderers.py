f430e5f5c4458c76cb468b25f353cd455d9299d1
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-06-08 10:19:18 +0200

    no need for extra renderer for the gallery section

--- miaiqi_cards_app/renderers.py
+++
@@ -1,8 +0,0 @@
-from simple_page import renderers
-from .models import Gallery
-
-
-@renderers.register(Gallery)
-class GalleryRenderer(renderers.SectionRenderer):
-    class Media:
-        css = dict(all=['cards/css/gallery.css'])
