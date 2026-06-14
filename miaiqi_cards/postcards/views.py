6f530fee522d7b89adeea24905cade7ea36543f1
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-06-14 11:32:04 +0200

    refactored postcard view

--- miaiqi_cards/postcards/views.py
+++ miaiqi_cards/postcards/views.py
@@ -8,9 +8,11 @@ def postcard(request, postcard_id, gallery_id=None):
     context = dict(postcard=get_object_or_404(Postcard, id=postcard_id))
     if gallery_id := request.GET.get('gallery_id'):
         gallery = get_object_or_404(Gallery, id=gallery_id)
-        gallery_postcards = list(gallery.postcards.all())
-        index = [p.pk for p in gallery_postcards].index(postcard_id)
-        context['previous_postcard'] = [None, *gallery_postcards, None][index]
-        context['next_postcard'] = [None, *gallery_postcards, None][index + 2]
+        postcards = list(gallery.postcards.all())
         context['gallery'] = gallery
+    else:
+        postcards = list(Postcard.objects.all())
+    index = [p.pk for p in postcards].index(postcard_id)
+    context['previous_postcard'] = [None, *postcards, None][index]
+    context['next_postcard'] = [None, *postcards, None][index + 2]
     return render(request, 'postcards/postcard.html', context)
