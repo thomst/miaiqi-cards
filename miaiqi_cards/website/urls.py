f64db55499aca94523e83c0f32b2dfd4e92cf79b
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-06-20 21:33:37 +0200

    removed obsolete urlpatterns

--- miaiqi_cards/website/urls.py
+++ miaiqi_cards/website/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from simple_page.views import page_view
 
 
 urlpatterns = [
     path('', page_view, name='page', kwargs={'slug': 'home'}),
-    path('postcard/', include('miaiqi_cards.postcards.urls')),
-    path('markdownx/', include('markdownx.urls')),
 ]
