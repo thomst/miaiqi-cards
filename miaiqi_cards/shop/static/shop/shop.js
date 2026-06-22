743e95443f0ea80de1d46088463b0b1a5678130c
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-06-22 18:17:07 +0200

    made the javascript more generic

--- miaiqi_cards/shop/static/shop/shop.js
+++ miaiqi_cards/shop/static/shop/shop.js
@@ -1,44 +1,64 @@
 (function() {
     "use strict";
 
-    function reloadFormset(event) {
-        event.preventDefault();
-        const form = document.querySelector('div.shop > form');
-        const addButton = form.querySelector('div.buttons input[type="button"]');
-        const csrftoken = form.querySelector('[name=csrfmiddlewaretoken]').value;
-        const totalForms = form.querySelector('div.shop input[id$=-TOTAL_FORMS');
-        const currentValue = parseInt(totalForms.value, 10);
-        totalForms.value = currentValue + 1;
+    function updateShop(html) {
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = html.trim();
+        document.querySelector('div.shop').replaceWith(wrapper.firstChild);
+        initButtons();
+    }
 
-        fetch(addButton.dataset.url, {
-            method: 'POST',
-            credentials: 'same-origin',
-            body: new FormData(form),
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-                'X-CSRFToken': csrftoken
-            }
-        })
+    function ajaxCall(event, data) {
+        event.preventDefault();
+
+        fetch(event.target.dataset.url, data)
         .then(function(response) {
-            if (!response.ok) throw new Error('Network response was not ok');
+            if (!response.ok) throw new Error(response.text());
             return response.text();
         })
-        .then(function(html) {
-            const wrapper = document.createElement('div');
-            wrapper.innerHTML = html.trim();
-            document.querySelector('div.shop').replaceWith(wrapper.firstChild);
-            initButtons();
-
-        })
+        .then(updateShop)
         .catch(function() {
             alert('Error reloading the formset.');
         });
     }
 
+    function getRequest(event) {
+        ajaxData = {
+            method: "GET",
+            credentials: 'same-origin',
+            headers: {'X-Requested-With': 'XMLHttpRequest'}
+        }
+        ajaxCall(event, ajaxData)
+    }
+
+    function postRequest(event) {
+        const form = document.querySelector('div.shop > form');
+        const csrftoken = form.querySelector('[name=csrfmiddlewaretoken]').value;
+        const ajaxData = {
+            method: "POST",
+            credentials: 'same-origin',
+            body: new FormData(form),
+            headers: {'X-Requested-With': 'XMLHttpRequest', 'X-CSRFToken': csrftoken}
+        }
+        ajaxCall(event, ajaxData)
+    }
+
+    const actions = {
+        addForm: function(event) {
+            event.preventDefault();
+            const totalForms = document.querySelector('div.shop input[id$=-TOTAL_FORMS');
+            const currentValue = parseInt(totalForms.value, 10);
+            totalForms.value = currentValue + 1;
+            postRequest(event)
+
+        }
+    }
+
     function initButtons() {
-        const addButton = document.querySelector('div.buttons input[type="button"]');
-        if (!addButton) return;
-        addButton.addEventListener('click', reloadFormset);
+        const buttons = document.querySelectorAll('div.buttons > input');
+        for (const button of buttons) {
+            button.addEventListener('click', actions[button.dataset.action]);
+        }
     }
 
     document.addEventListener('DOMContentLoaded', initButtons);
