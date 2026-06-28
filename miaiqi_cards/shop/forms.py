038dd677c811ccb194e856eb3c93b9b0a16c9e37
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-06-28 18:06:37 +0200

    added placeholder for cart-items-form

--- miaiqi_cards/shop/forms.py
+++ miaiqi_cards/shop/forms.py
@@ -3,11 +3,21 @@ from miaiqi_cards.postcards.models import Postcard
 
 
 class EmailForm(forms.Form):
-    email_address = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email Adresse'}))
-    repeated_email_address = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Nochmal ;-)'}))
+    email_address = forms.EmailField(
+        widget=forms.TextInput(attrs={'placeholder': 'Email Adresse'}),
+        )
+    repeated_email_address = forms.EmailField(
+        widget=forms.TextInput(attrs={'placeholder': 'Email Adresse wiederholen'}),
+        )
 
 
 class CartItemForm(forms.Form):
-    # Choices will be set by the shop section renderer.
-    product = forms.ModelChoiceField(Postcard.objects.all(), required=True)
-    quantity = forms.IntegerField(min_value=1, required=True)
+    # Field will be updated by the shop-view.
+    product = forms.ModelChoiceField(
+        Postcard.objects.all(),
+        widget=forms.Select(attrs={'placeholder': 'Postkarte'}),
+        )
+    quantity = forms.IntegerField(
+        min_value=1,
+        widget=forms.NumberInput(attrs={'placeholder': 'Anzahl'}),
+        )
