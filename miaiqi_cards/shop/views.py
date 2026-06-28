02c4ecef76c7bc7982cf689b16f625fd7f75b445
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-06-28 12:45:34 +0200

    refactored shop views

--- miaiqi_cards/shop/views.py
+++ miaiqi_cards/shop/views.py
@@ -4,6 +4,8 @@ from django.template.loader import render_to_string
 from django.utils.functional import cached_property
 from django.http import HttpResponse
 from cart.cart import Cart, InvalidDiscountError
+from cart.session import DjangoSessionAdapter
+from cart.cart import CART_ID
 from . import models
 from . import forms
 
@@ -11,8 +13,7 @@ from . import forms
 def set_state(state):
     def decorator(method):
         def wrapper(self, *args, **kwargs):
-            # self.request.session['shop-state'] = state
-            self.state = state
+            self.request.session['shop-state'] = state
             return method(self, *args, **kwargs)
         return wrapper
     return decorator
@@ -36,39 +37,14 @@ class ShopView:
     def state(self):
         return self.request.session.get('shop-state')
 
+    # FIXME: Use a get_formset_class with extra argument.
     @property
-    def old_state(self):
-        return self.request.session.get('old-shop-state')
-
-    @state.setter
-    def state(self, value):
-        self.request.session['old-shop-state'] = self.state
-        self.request.session['shop-state'] = value
-
-    @cached_property
     def formset_class(self):
         # FIXME: Update limit_choices_to on formfield instead of creating new field and form.
         field = ModelChoiceField(self.shop.gallery.postcards.all(), required=True)
         form_class = type('CartItemForm', (forms.CartItemForm,), dict(product=field))
         extra = 1 if self.cart.is_empty() else 0
         return formset_factory(form_class, extra=extra, min_num=1, validate_min=True)
-
-    @cached_property
-    def formset(self):
-        if self.request.POST and self.old_state == self.ORDER_STATE:
-            return self.formset_class(self.request.POST)
-        elif not self.cart.is_empty():
-            initial = [dict(product=i.product.pk, quantity=i.quantity) for i in self.cart]
-            return self.formset_class(initial=initial)
-        else:
-            return self.formset_class()
-
-    @cached_property
-    def email_form(self):
-        if self.request.POST and self.old_state == self.CHECKOUT_STATE:
-            return forms.EmailForm(self.request.POST)
-        else:
-            return forms.EmailForm()
 
     def setup_cart(self, data):
         self.cart.clear()
@@ -88,30 +64,57 @@ class ShopView:
 
     @set_state(ORDER_STATE)
     def get_order_html(self):
-        return render_to_string('shop/order.html', dict(shop=self), self.request)
+        if 'new' in self.request.GET:
+            DjangoSessionAdapter(self.request).delete(CART_ID)
+            formset = self.formset_class()
+        elif 'checkout' in self.request.GET:
+            formset = self.formset_class(self.request.POST)
+        elif not self.cart.is_empty():
+            initial = [dict(product=i.product.pk, quantity=i.quantity) for i in self.cart]
+            formset = self.formset_class(initial=initial)
+        else:
+            formset = self.formset_class()
+        context = dict(shop=self, formset=formset)
+        return render_to_string('shop/order.html', context, self.request)
 
     @set_state(CHECKOUT_STATE)
     def get_checkout_html(self):
-        if self.old_state in [self.CHECKOUT_STATE, self.CONFIRMATION_STATE]:
-            return render_to_string('shop/checkout.html', dict(shop=self), self.request)
-        elif self.formset.is_valid():
-            self.setup_cart(self.formset.cleaned_data)
-            return render_to_string('shop/checkout.html', dict(shop=self), self.request)
+        if 'checkout' in self.request.GET:
+            formset = self.formset_class(self.request.POST)
+            if formset.is_valid():
+                self.setup_cart(formset.cleaned_data)
+                email_form = forms.EmailForm()
+                context = dict(shop=self, cart=self.cart, email_form=email_form)
+                return render_to_string('shop/checkout.html', context, self.request)
+            else:
+                return self.get_order_html()
+        elif 'buy' in self.request.GET:
+            email_form = forms.EmailForm(self.request.POST)
+            context = dict(shop=self, cart=self.cart, email_form=email_form)
+            return render_to_string('shop/checkout.html', context, self.request)
         else:
-            return self.get_order_html()
+            email_form = forms.EmailForm()
+            context = dict(shop=self, cart=self.cart, email_form=email_form)
+            return render_to_string('shop/checkout.html', context, self.request)
 
     @set_state(CONFIRMATION_STATE)
     def get_confirmation_html(self):
-        if self.email_form.is_valid():
-            # TODO: Send confirmation email.
-            self.cart.checkout()
-            render_to_string('shop/confirmation.html', dict(shop=self), self.request)
+        if 'buy' in self.request.GET:
+            email_form = forms.EmailForm(self.request.POST)
+            if email_form.is_valid():
+                # TODO: Send confirmation email.
+                self.cart.checkout()
+                context = dict(shop=self, cart=self.cart)
+                return render_to_string('shop/confirmation.html', context, self.request)
+            else:
+                return self.get_checkout_html()
         else:
-            return self.get_checkout_html()
+            context = dict(shop=self, cart=self.cart)
+            return render_to_string('shop/confirmation.html', context, self.request)
 
-    @cached_property
+    @property
     def html(self):
-        if not self.state or self.state == self.ORDER_STATE:
+        if self.state == self.ORDER_STATE or not self.state:
             return self.get_order_html()
         if self.state == self.CHECKOUT_STATE:
             return self.get_checkout_html()
