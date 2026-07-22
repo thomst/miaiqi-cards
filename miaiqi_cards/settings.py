f642a1aaae8754423fdc0ad822b415d49e3410c1
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-07-22 22:07:06 +0200

    no need for os.path.join

--- miaiqi_cards/settings.py
+++ miaiqi_cards/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,4 +131,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'miaiqi_cards/staticfiles/')
+STATIC_ROOT = BASE_DIR / 'miaiqi_cards/staticfiles/'
