5616e60c12ed8e16267d00a9b22ee3fa38ad3d1c
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-08-07 11:45:11 +0200

    refactored the postcards navigation

--- miaiqi_cards/postcards/views.py
+++ miaiqi_cards/postcards/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import get_object_or_404, render
+from ..website.models import MiaiqiCardsPage
 from .models import GallerySection
 from .models import Postcard
 
 
 def postcard(request, postcard_id):
+    postcards = Postcard.objects.all()
     postcard = get_object_or_404(Postcard, id=postcard_id)
     context = dict(postcard=postcard)
 
-    if section_id := request.GET.get('section_id'):
-        section = get_object_or_404(GallerySection, id=section_id)
-        postcards = list(section.postcards.all())
-        context['section'] = section
+    try:
+        page = MiaiqiCardsPage.objects.get(slug='home')
+    except MiaiqiCardsPage.DoesNotExist:
+        pass
     else:
-        postcards = list(Postcard.objects.all())
+        context['page'] = page
+        gallery_sections = page.sections.instance_of(GallerySection)
+        if gallery_sections:
+            context['section'] = gallery_sections[0]
+            postcards = gallery_sections[0].postcards.all()
 
     index = [p.pk for p in postcards].index(postcard_id)
     context['previous_postcard'] = [None, *postcards, None][index]
